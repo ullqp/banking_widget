1d30c54006acff265695e87cd755370eaf250d05
Ksenia Baranova <waynaungelneal@gmail.com>
2025-02-04 12:30:03 +0300

    сделано оформление кода

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,22 @@
-def filter_by_state(list_dictionary: list, state: str='EXECUTED') -> list:
+def filter_by_state(operations_list: list, state: str = "EXECUTED") -> list:
     """Функция, которая фильтрует данные о банковских операциях."""
-    new_dictionary: list = list()
-    for i in list_dictionary:
-        if i['state'] == state:
-            new_dictionary.append(i)
-    return new_dictionary
+    new_list: list = list()
+    for i in operations_list:
+        if i["state"] == state:
+            new_list.append(i)
+    return new_list
 
 
-def sort_by_date(list_dictionary: list, reverse: str='True') -> list:
+def sort_by_date(operations_list: list, reverse: str = "True") -> list:
     """Функция, которая сортирует операции по дате."""
-    new_dictionary: list = sorted(list_dictionary, key=lambda x: x['date'][:4]+x['date'][5:7]+x['date'][8:10]
-                                                           +x['date'][11:13]+x['date'][14:16]+x['date'][17:19],
-                                  reverse=reverse=='True')
-    return new_dictionary
+    new_list: list = sorted(
+        operations_list,
+        key=lambda x: x["date"][:4]
+        + x["date"][5:7]
+        + x["date"][8:10]
+        + x["date"][11:13]
+        + x["date"][14:16]
+        + x["date"][17:19],
+        reverse=reverse == "True",
+    )
+    return new_list
