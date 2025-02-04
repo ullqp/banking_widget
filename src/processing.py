2450ee29a4b6ad6b2038c8be66034e0e643bef0f
Ksenia Baranova <waynaungelneal@gmail.com>
2025-02-04 15:19:45 +0300

    Исправлены аннотации типов и наименнования переменных

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,22 @@
 def filter_by_state(operations_list: list, state: str = "EXECUTED") -> list:
     """Функция, которая фильтрует данные о банковских операциях."""
     new_list: list = list()
-    for i in operations_list:
-        if i["state"] == state:
-            new_list.append(i)
+    for operation in operations_list:
+        if operation["state"] == state:
+            new_list.append(operation)
     return new_list
 
 
-def sort_by_date(operations_list: list, reverse: str = "True") -> list:
+def sort_by_date(operations_list: list, reverse: bool = True) -> list:
     """Функция, которая сортирует операции по дате."""
     new_list: list = sorted(
         operations_list,
-        key=lambda x: x["date"][:4]
-        + x["date"][5:7]
-        + x["date"][8:10]
-        + x["date"][11:13]
-        + x["date"][14:16]
-        + x["date"][17:19],
-        reverse=reverse == "True",
+        key=lambda operation: operation["date"][:4]
+        + operation["date"][5:7]
+        + operation["date"][8:10]
+        + operation["date"][11:13]
+        + operation["date"][14:16]
+        + operation["date"][17:19],
+        reverse=reverse == True,
     )
     return new_list
