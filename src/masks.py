b6d9759ebf942b0ee5bba5d37f47e3a4237f2c5c
ullqp <waynaungelneal@gmal.com>
2025-02-20 17:06:16 +0300

    Сохранение изменений перед слиянием

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номер карты."""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    if len(card_number) == 16 and card_number.isdigit():
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return "Некорректный номер карты."
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция, которая маскирует номер счёта."""
-    return f"**{account_number[-4:]}"
+    if len(account_number) > 5 and account_number.isdigit():
+        return f"**{account_number[-4:]}"
+    else:
+        return "Некорректный номер счёта."
--- src/processing.py
+++ src/processing.py
@@ -2,13 +2,29 @@ def filter_by_state(operations_list: list, state: str = "EXECUTED") -> list:
     """Функция, которая фильтрует данные о банковских операциях."""
     new_list: list = list()
     for operation in operations_list:
-        if operation["state"] == state:
+        if "state" in operation.keys():
+            if operation["state"] == state:
+                new_list.append(operation)
+        else:
             new_list.append(operation)
     return new_list
 
 
 def sort_by_date(operations_list: list, reverse: bool = True) -> list:
     """Функция, которая сортирует операции по дате."""
+    for operation in operations_list:
+        numbers: str = (
+            operation["date"][:4]
+            + operation["date"][5:7]
+            + operation["date"][8:10]
+            + operation["date"][11:13]
+            + operation["date"][14:16]
+            + operation["date"][17:19]
+        )
+        if not numbers.isdigit():
+            operations_list.remove(operation)
+            print(operations_list)
+
     new_list: list = sorted(
         operations_list,
         key=lambda operation: operation["date"][:4]
